85943fff44d2cd1fe3770df566d3be8000c6d12e
jonatan timoteo dos santos <jonatan_ts@hotmail.com>
2022-05-26 00:50:08 -0300

    create header

---
+++ Incicle/src/components/Header/index.jsx
@@ -0,0 +1,14 @@
+import styles from './style.module.css'
+import logo from '../../../assets/logo.png'
+
+const Header = () => {
+  return (
+    <header className={styles.container}>
+      <div className={styles.main}>
+        <img className={styles.logo} src={logo} alt="logo" />
+      </div>  
+    </header>
+  )
+}
+
+export default Header
