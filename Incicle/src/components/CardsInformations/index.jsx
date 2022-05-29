58d1b09d8bf2186eb68372c27c753de1ee2b71b3
jonatan timoteo dos santos <jonatan_ts@hotmail.com>
2022-05-29 14:50:01 -0300

    list card initialized

--- Incicle/src/components/CardsInformations/index.jsx
+++ Incicle/src/components/CardsInformations/index.jsx
@@ -1,7 +1,17 @@
 import styles from './CardsInformations.module.css'
 import poligono from '../../../assets/Polígono.png' 
+import { useState } from 'react'
 
 const CardsInformations = () => {
+
+  const [listCard, setListCard] = useState([])
+
+  fetch('../../../data.json')
+    .then(response => response.json())
+    .then( data  => setListCard(data))
+
+
+
   return (
     <div className={styles.container}>
       <div className={styles.gridCardItems}>
@@ -17,10 +27,25 @@ const CardsInformations = () => {
           </div>
 
           <ul className={styles.list}>
-            <li>
-              <div className={styles.informationCard}>
-                <img src='https://google' alt="" />
+            <li className={styles.listCard}>
+
+              <div className={styles.imageCard}>
+                <img src='' alt="capa do card" />
+                <div className={styles.informationCard}>
+
+                  <h3>Geral dispensado para curtir o feriado</h3>
+                  <div>
+                    <span className={styles.event}>Comunicado</span>
+                    <span className={styles.data}>13 de out</span>
+                  </div>
+
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas a repudiandae ab veritatis similique...</p>
+
+                </div>
               </div>
+
+              <span>...</span>
+
             </li>
           </ul>
         </section>
@@ -29,14 +54,16 @@ const CardsInformations = () => {
 
           <div className={styles.cardEndomarketing}>
             <h2>Endomarketing</h2>
-            <p>Endomarketing está relacionado às 
+            <p>
+              Endomarketing está relacionado às 
               ações de treinamento ou qualificação
               dos colaboradores da empresa
               visando um melhor serviço para o
               cliente. Marketing interno, devido ao
               nome, é usualmente confundido com
               Endomarketing mesmo sendo
-              conceitos diferentes.</p>
+              conceitos diferentes.
+            </p>
             <button>DISPENSAR</button>
           </div>
 
