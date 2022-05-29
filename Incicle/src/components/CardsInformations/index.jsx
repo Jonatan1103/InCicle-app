36061900b430f9dfbb5b7bdeabd619ac2af98d29
jonatan timoteo dos santos <jonatan_ts@hotmail.com>
2022-05-29 10:35:06 -0300

    Endomarketing finalized

--- Incicle/src/components/CardsInformations/index.jsx
+++ Incicle/src/components/CardsInformations/index.jsx
@@ -1,17 +1,18 @@
 import styles from './CardsInformations.module.css'
+import poligono from '../../../assets/Polígono.png' 
 
 const CardsInformations = () => {
   return (
     <div className={styles.container}>
       <div className={styles.gridCardItems}>
-        <section className={styles.cardItem}>
+        <section className={styles.cardItemPrimary}>
 
           <div className={styles.header}>
             <h1>Endomarketing</h1>
 
-            <div>
-              <button>TIPO \/</button>
-              <button>CRIAR</button>
+            <div className={styles.buttons}>
+              <div className={styles.buttonType}>TIPO <img src={poligono}/></div>
+              <div className={styles.buttonCrate}>CRIAR <span>+</span></div>
             </div>
           </div>
 
@@ -23,19 +24,20 @@ const CardsInformations = () => {
         </section>
 
         <section className={styles.cardItemSecundary}>
-          <h2>Endomarketing</h2>
-          <p>
-            Endomarketing está relacionado às
-            ações de treinamento ou qualificação
-            dos colaboradores da empresa
-            visando um melhor serviço para o
-            cliente. Marketing interno, devido ao
-            nome, é usualmente confundido com
-            Endomarketing mesmo sendo
-            conceitos diferentes.
-          </p>
 
-          <button>DISPENSAR</button>
+          <div className={styles.cardEndomarketing}>
+            <h2>Endomarketing</h2>
+            <p>Endomarketing está relacionado às 
+              ações de treinamento ou qualificação
+              dos colaboradores da empresa
+              visando um melhor serviço para o
+              cliente. Marketing interno, devido ao
+              nome, é usualmente confundido com
+              Endomarketing mesmo sendo
+              conceitos diferentes.</p>
+            <button>DISPENSAR</button>
+          </div>
+
         </section>
       </div>
     </div>
